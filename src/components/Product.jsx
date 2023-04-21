49d8857586ce5809d7a956764ed5f20ba33cde17
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-21 12:44:40 +0800

    added product photo and description

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Product = () => {
   return (
-    <div className="h-screen w-full bg-emerald-400 flex flex-row items-center justify-center gap-16">
-      <img src="m-lime.png" alt="" className="object-cover w-80" />
+    <div className="h-screen w-full bg-[#F0C83D] flex flex-row items-center justify-center gap-16">
+      <img src="p.webp" alt="" className="object-cover w-80" />
       <div className="flex flex-col items-center w-1/4 justify-center">
-        <h1 className="text-5xl font-semibold mb-12">Lime Hibiscus Rooibos Tea</h1>
-        <hr class="border-t-2 border-gray-300 my-8" />
-        <p>
-          Organic rooibos tea combined with ruby-red hibiscus and rosehips give
-          a lemony-tart, berry-rich bite and maintains a healthy bod with every
-          sip. A twist of lime plus bubbles makes this naturally caffeine-free
-          tea a refreshing, all-day sipper.
+        <h1 className="text-5xl font-semibold mb-12">Peach Oolong Tea</h1>
+        <p className="text-xl">
+          The marriage of sweet peach with oolong is brought to life with
+          effervescence! All natural tea and real fruit juice lets you identify
+          and taste the refreshing flavors.
         </p>
       </div>
     </div>
--- src/components/Product1.jsx
+++ src/components/Product1.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Product1 = () => {
   return (
-    <div className="h-screen w-full product1 flex flex-row items-center justify-center gap-16">
-      <img src="m-pineapple.png" alt="" className="object-cover w-80" />
+    <div className="h-screen w-full bg-[#B6A5C8] flex flex-row items-center justify-center gap-16">
+      <img src="p1.webp" alt="" className="object-cover w-80" />
       <div className="flex flex-col items-center w-1/4 justify-center">
-        <h1 className="text-5xl font-semibold mb-12">Lime Hibiscus Rooibos Tea</h1>
-        <hr class="border-t-2 border-gray-300 my-8" />
-        <p>
-          Organic rooibos tea combined with ruby-red hibiscus and rosehips give
-          a lemony-tart, berry-rich bite and maintains a healthy bod with every
-          sip. A twist of lime plus bubbles makes this naturally caffeine-free
-          tea a refreshing, all-day sipper.
+        <h1 className="text-5xl font-semibold mb-12">Blackberry Purple Tea</h1>
+        <p className="text-xl">
+          A unique blend which embodies a royal purple hue is set to tease your
+          tastebuds with the sweet and tart notes of the blackberry. Purple tea
+          also has the most antioxidants than any other tea leaf!
         </p>
       </div>
     </div>
