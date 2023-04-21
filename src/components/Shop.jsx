4cb62507b64b052f18f80ec3b5cf313c125236eb
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-22 01:10:42 +0800

    minor change

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,8 +5,8 @@ const Shop = () => {
   return (
     <div className='h-screen bg-[#BADAD5] flex flex-col justify-center items-center'>
       <img src="dog.jpg" alt="" className='h-40'/>
-      <h1 className='text-4xl'>Work in progress</h1>
-      <Link to='/' className='mt-10 bg-[#69a49b] rounded-lg text-2xl p-4'>Back to home</Link>
+      <h1 className='text-4xl text-[#484180]'>Work in progress</h1>
+      <Link to='/' className='mt-10 text-[#484180] rounded-lg text-2xl p-4'>Back to home</Link>
     </div>
   )
 }
