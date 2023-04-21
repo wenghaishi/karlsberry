7a71136afa9691c973a5829227dd43be7500623e
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-21 23:11:32 +0800

    mobile responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,13 +4,13 @@ import Logo from "./Logo";
 
 const HeroSection = () => {
   return (
-    <div className="h-screen md:h-screen hero z-20 flex flex-col justify-center items-center">
+    <div className="pt-96  lg:pt-0 md:h-screen hero z-20 flex flex-col justify-center items-center">
       <Parallax className="z-20">
         <div className="flex flex-col justify-center mt-40 items-center gap-16">
-          <h1 className="z-40 md:text-6xl text-slate-600">
+          <h1 className="z-40 lg:text-6xl text-slate-600">
             Your favourite sparkling tea!
           </h1>
-          <p className=" text-sm md:text-2xl w-1/2 z-20">
+          <p className=" text-sm lg:text-2xl w-1/2 z-20">
             Cold brewed with organic tea, fruit juices, botanical infusions and
             antioxidants without sugar or sweeteners to keep you healthy, fun &
             sparkling.
