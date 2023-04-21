11253e33bd8ce16656886c4289ab79ec2c8203aa
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-21 23:16:12 +0800

    responsive for product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Product = (props) => {
   return (
-    <div className={`h-screen w-full z-20 flex flex-row items-center justify-center gap-6 md:gap-16 ${ props.color}`}>
+    <div className={`h-96 md:h-screen w-full z-20 flex flex-row items-center justify-center gap-6 md:gap-16 ${ props.color}`}>
       <img src={props.img} alt="" className="object-cover flex w-3/12 md:max-w-5xl" />
       <div className="flex flex-col w-2/4 md:w-1/4 justify-center">
         <h1 className={`md:text-5xl text-lg font-semibold mb-12 text-left ${ props.text}`}>{props.name}</h1>
