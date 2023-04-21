a48646f938c379469364280f9725a76107153740
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-22 00:01:51 +0800

    minor changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     <div className="pt-96 xl:pt-0 md:h-screen hero z-20 flex flex-col justify-center items-center">
       <Parallax className="z-20">
         <div className="flex flex-col justify-center mt-40 items-center gap-10">
-          <h1 className="z-40 md:text-3xl xl:text-6xl text-slate-600">
+          <h1 className="z-40 md:text-4xl xl:text-6xl text-[#484180]">
             Your favourite sparkling tea!
           </h1>
           <p className=" text-sm lg:text-lg xl:text-2xl w-1/2 z-20">
