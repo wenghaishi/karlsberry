b612e565999859cd622731e4acacb889147387c8
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-22 01:08:57 +0800

    changed color

--- src/components/RootEnd.jsx
+++ src/components/RootEnd.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const RootEnd = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen root-end">
-      <div className="text-lg md:text-5xl text-center font-semibold text-green-800 ">
+      <div className="text-lg md:text-5xl text-center font-semibold text-zinc-300 ">
         Karlsberry advocates for youth & children.
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center pd-4 gap-16 p-10 md:p-14 root-end">
         <img src="children.webp" alt="" className="max-w-md	w-2/3 flex rounded-xl" />
-        <div className="w-5/6 md:w-1/3 text-green-800 text-sm md:text-md lg:text-lg">
+        <div className="w-5/6 md:w-1/3 text-zinc-300 text-sm md:text-md lg:text-lg">
           We pledge to donate 1% of net sales to non-profits that support
           disadvantaged and vulnerable communities of youth and children.
           <br></br>
