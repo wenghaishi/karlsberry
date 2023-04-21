94954b6b84c1bcb438c6dc85c34fdaf464210409
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-22 00:03:33 +0800

    minor change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiOutlineInstagram } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="flex h-14 flex-row justify-center items-center footer gap-4 bottom-0">
-      <a href="https://www.instagram.com/sylvesterpillay"><AiOutlineInstagram className="text-2xl text-zinc-200"/></a>
+      <a href="https://www.instagram.com/karlsberry"><AiOutlineInstagram className="text-2xl text-zinc-200"/></a>
       <div className=" text-zinc-200"> Brewing soon in Singapore. 2023.</div>
     </div>
   );
