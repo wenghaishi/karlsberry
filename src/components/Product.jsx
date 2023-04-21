c55a182a2e1bc6e780a50fd42d0fce6a83520859
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-21 13:50:47 +0800

    refactor products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Product = () => {
+const Product = (props) => {
   return (
-    <div className="h-screen w-full bg-[#F0C83D] flex flex-row items-center justify-center gap-16">
-      <img src="p.webp" alt="" className="object-cover w-80" />
-      <div className="flex flex-col items-center w-1/4 justify-center">
-        <h1 className="text-5xl font-semibold mb-12">Peach Oolong Tea</h1>
-        <p className="text-xl">
+    <div className={`h-screen w-full flex flex-row items-center justify-center gap-16 ${ props.color}`}>
+      <img src={props.img} alt="" className="object-cover w-80" />
+      <div className="flex flex-col  w-1/4 justify-center">
+        <h1 className={`text-5xl font-semibold mb-12 text-left ${ props.text}`}>{props.name}</h1>
+        <p className={`text-xl ${ props.text}`}>
           The marriage of sweet peach with oolong is brought to life with
           effervescence! All natural tea and real fruit juice lets you identify
           and taste the refreshing flavors.
--- src/components/RootPage.jsx
+++ src/components/RootPage.jsx
@@ -5,23 +5,81 @@ import HeroSection from "./HeroSection";
 import Product from "./Product";
 import Product1 from "./Product1";
 import Footer from "./Footer";
+import Product2 from "./Product2";
 
 const RootPage = () => {
+  const products = [
+    {
+      name: "Peach Oolong",
+      description:
+        "The marriage of sweet peach with oolong is brought to life with effervescence! All natural tea and real fruit juice lets you identify and taste the refreshing flavors.",
+      bgColor: "bg-[#F5D467]",
+      img: "p.webp",
+      text: "text-[#CC7722]",
+    },
+    {
+      name: "Blackberry Purple",
+      description:
+        "A unique blend which embodies a royal purple hue is set to tease your tastebuds with the sweet and tart notes of the blackberry. Purple tea also has the most antioxidants than any other tea leaf!",
+      bgColor: "bg-[#B6A5C8]",
+      img: "p1.webp",
+      text: "text-[#0A0B21]",
+    },
+    {
+      name: "Rose White",
+      description:
+        "You will love the refreshing taste of alphonso mango balanced with the earthiness of black tea. Light on your palette yet full-bodied. Black tea improves focus and lowers blood sugar levels.",
+      bgColor: "bg-[#ECC5DC]",
+      img: "p2.webp",
+      text: "text-[#C4478F]",
+    },
+    {
+      name: "Mango Black",
+      description:
+        "You will love the refreshing taste of alphonso mango balanced with the earthiness of black tea. Light on your palette yet full-bodied. Black tea improves focus and lowers blood sugar levels.",
+      bgColor: "bg-[#9E8C6E]",
+      img: "p3.webp",
+      text: "text-[#352423]",
+    },
+    {
+      name: "Elderflower Green Tea",
+      description:
+        "Move aside jasmine green tea! We steep elderflowers instead to infuse the unique sweet floral notes into the popular tea. Green tea boosts your metabolism and is high in polyphenols.",
+      bgColor: "bg-[#D0DE7F]",
+      img: "p4.webp",
+      text: "text-[#185A37]",
+    },
+    {
+      name: "Apple Rooibos Tea",
+      description:
+        "Consciously blended with children in mind and for adults who prefer decaf, we steep caffeine-free rooibos leaves that produces a ravishing red color and flavor it with natural gala apple juice.",
+      bgColor: "bg-[#EA8C7C]",
+      img: "p5.webp",
+      text: "text-[#A11C16]",
+    },
+  ];
   return (
     <div className="flex flex-col">
-        <Banner />
-        <div className="relative inline z-50">
-          <img
-            src="bg.jpg"
-            alt=""
-            className="absolute object-cover mt-0 h-screen"
-          />
-          <Navbar />
-        </div>
-        <HeroSection />
-        <Product />
-        <Product1 />
-        <Footer />
+      <Banner />
+      <div className="relative inline z-50">
+        <img
+          src="bg.jpg"
+          alt=""
+          className="absolute object-cover mt-0 h-screen"
+        />
+        <Navbar />
+      </div>
+      <HeroSection />
+      {products.map((product) => (
+        <Product
+          name={product.name}
+          description={product.description}
+          color={product.bgColor}
+          img={product.img}
+          text={product.text}
+        />
+      ))}
+      <Footer />
     </div>
   );
 };
